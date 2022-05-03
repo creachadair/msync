12207927a2b2a0432e54b20597e2e26d94f45ff0
M. J. Fromberger <michael.j.fromberger@gmail.com>
2022-05-02 17:36:15 -0700

    Add package msync.

---
+++ msync.go
@@ -0,0 +1,64 @@
+// Package msync defines some helpful types for managing concurrency.
+package msync
+
+import "sync"
+
+// Trigger is an edge-triggered condition. It is analogous in effect to the
+// standard sync.Cond type, but uses a channel for signaling so that the
+// waiters can select on its completion.
+//
+// A zero value is ready for use but must not be copied after first use.
+type Trigger struct {
+	μ  sync.Mutex
+	ch chan struct{}
+
+	// The signal channel is lazily initialized by the first waiter.
+}
+
+// NewTrigger constructs a new unready Trigger.
+func NewTrigger() *Trigger { return new(Trigger) }
+
+// Signal wakes all pending waiters and resets the trigger.
+func (t *Trigger) Signal() {
+	t.μ.Lock()
+	defer t.μ.Unlock()
+	if t.ch == nil {
+		// There are no goroutines waiting, so there is nothing to do.
+		return
+	}
+	close(t.ch)
+	t.ch = nil
+}
+
+// Ready returns a channel that is closed when t is signaled.
+func (t *Trigger) Ready() <-chan struct{} {
+	t.μ.Lock()
+	defer t.μ.Unlock()
+	if t.ch == nil {
+		t.ch = make(chan struct{})
+	}
+	return t.ch
+}
+
+// Handoff is a singly-buffered level-triggered rendezvous channel. A waiter
+// blocks on the Ready channel until a value is provided by Send. A value
+// handed off persists until consumed.
+type Handoff struct {
+	ch chan interface{}
+}
+
+// NewHandoff constructs a new empty handoff.
+func NewHandoff() Handoff { return Handoff{ch: make(chan interface{}, 1)} }
+
+// Send provides a value to handoff.  Send does not block.  Once v is sent to
+// h, subsequent calls to Send will be discarded until a receiver accepts the
+// handoff of v.
+func (h *Handoff) Send(v interface{}) {
+	select {
+	case h.ch <- v:
+	default:
+	}
+}
+
+// Ready returns a channel that delivers a value when a handoff is available.
+func (h *Handoff) Ready() <-chan interface{} { return h.ch }
---
+++ msync_test.go
@@ -0,0 +1,97 @@
+package msync_test
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/creachadair/msync"
+)
+
+func TestTrigger(t *testing.T) {
+	// Start up a bunch of tasks that listen to a trigger, signal the trigger,
+	// and verify that it woke them all up.
+	tr := msync.NewTrigger()
+
+	const numTasks = 3
+
+	ok := make([]bool, numTasks)
+	var start, stop sync.WaitGroup
+
+	for i := 0; i < numTasks; i++ {
+		i := i
+		start.Add(1)
+		stop.Add(1)
+		go func() {
+			ch := tr.Ready()
+			start.Done()
+			<-ch
+			ok[i] = true
+			stop.Done()
+		}()
+	}
+
+	start.Wait()
+	tr.Signal()
+	stop.Wait()
+
+	for i, b := range ok {
+		if !b {
+			t.Errorf("Task %d did not report success", i+1)
+		}
+	}
+}
+
+func TestHandoff(t *testing.T) {
+	h := msync.NewHandoff()
+
+	// Multiple sends to h do not block.
+	h.Send(1)
+	h.Send(2)
+	h.Send(3)
+
+	// The first value handed off is ready.
+	if got := <-h.Ready(); got != 1 {
+		t.Errorf("Ready: got %v, want 1", got)
+	}
+
+	// Now that the value has been handed off, nothing is available till we send
+	// another value.
+	select {
+	case <-time.After(100 * time.Millisecond):
+		// OK, nothing here
+	case bad := <-h.Ready():
+		t.Errorf("Ready: unexpected value: %v", bad)
+	}
+
+	// The next value sent is the next received.
+	h.Send(4)
+	if got := <-h.Ready(); got != 4 {
+		t.Errorf("Ready: got %v, want 4", got)
+	}
+
+	// Play ping-ping.
+	p := msync.NewHandoff()
+	done := make(chan struct{})
+	var sum int
+	go func() {
+		defer close(done)
+		for i := 0; i < 3; i++ {
+			v := <-h.Ready()
+			sum += v.(int)
+			p.Send(nil)
+		}
+	}()
+
+	h.Send(1)
+	<-p.Ready()
+	h.Send(3)
+	<-p.Ready()
+	h.Send(5)
+	<-p.Ready()
+	<-done
+
+	if sum != 9 {
+		t.Errorf("Checksum: got %v, want 9", sum)
+	}
+}

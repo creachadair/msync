92bf075d3b9b969e79ef4e729fe7cfa897b07a15
M. J. Fromberger <fromberger@tailscale.com>
2022-12-03 11:41:17 -0800

    Expand Trigger documentation.

--- msync.go
+++ msync.go
@@ -6,12 +6,16 @@ import (
 	"sync"
 )
 
-// Trigger is an edge-triggered condition shared by multiple waiting
-// goroutines.  It is analogous in effect to the standard condition variable
-// (sync.Cond) type, but uses a channel for signaling so that the waiters can
-// select on its completion.
+// Trigger is an edge-triggered condition shared by multiple goroutines.  It is
+// analogous to the standard condition variable (sync.Cond) but uses a channel
+// for signaling.
 //
-// A zero value is ready for use but must not be copied after first use.
+// To wait on the trigger, call Ready to obtain a channel. This channel will be
+// closed the next time the Signal method is executed. A fresh channel is
+// created after each call to Signal, and is shared among all calls to Ready
+// that occur in the window between signals.
+//
+// A zero Trigger is ready for use but must not be copied after first use.
 type Trigger struct {
 	μ  sync.Mutex
 	ch chan struct{}
@@ -26,18 +30,22 @@ func NewTrigger() *Trigger { return new(Trigger) }
 func (t *Trigger) Signal() {
 	t.μ.Lock()
 	defer t.μ.Unlock()
-	if t.ch == nil {
-		// There are no goroutines waiting, so there is nothing to do.
-		return
+
+	// If the channel is nil, there are no goroutines waiting.  The next waiter
+	// will update the channel (see Ready).
+	if t.ch != nil {
+		close(t.ch)
+		t.ch = nil
 	}
-	close(t.ch)
-	t.ch = nil
 }
 
 // Ready returns a channel that is closed when t is signaled.
 func (t *Trigger) Ready() <-chan struct{} {
 	t.μ.Lock()
 	defer t.μ.Unlock()
+
+	// The first waiter after construction or a signal lazily populates the
+	// channel for the next period.
 	if t.ch == nil {
 		t.ch = make(chan struct{})
 	}

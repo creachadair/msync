e0b580c4fab7acfc62637a1f103dbd0f2b22447c
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-09-08 10:13:42 -0700

    msync: add Set and Reset methods to the Trigger
    Reframe Signal as a combination of Set and Reset.
    Set activates the trigger and leaves it active,
    Reset resets and deactivates the trigger.

--- msync_test.go
+++ msync_test.go
@@ -16,37 +16,93 @@ import (
 func TestTrigger(t *testing.T) {
 	defer leaktest.Check(t)()
 
-	// Start up a bunch of tasks that listen to a trigger, signal the trigger,
-	// and verify that it woke them all up.
-	tr := msync.NewTrigger()
-
-	const numTasks = 3
-
-	ok := make([]bool, numTasks)
-	var start, stop sync.WaitGroup
-
-	for i := 0; i < numTasks; i++ {
-		i := i
-		start.Add(1)
-		stop.Add(1)
-		go func() {
-			ch := tr.Ready()
-			start.Done()
-			<-ch
-			ok[i] = true
-			stop.Done()
-		}()
-	}
-
-	start.Wait()
-	tr.Signal()
-	stop.Wait()
-
-	for i, b := range ok {
-		if !b {
-			t.Errorf("Task %d did not report success", i+1)
+	checkNotActive := func(t *testing.T, tr *msync.Trigger) {
+		select {
+		case <-tr.Ready():
+			t.Error("Trigger is active when it should not be")
+		default:
 		}
 	}
+
+	t.Run("Signal", func(t *testing.T) {
+		// Start up a bunch of tasks that listen to a trigger, signal the trigger,
+		// and verify that it woke them all up.
+		tr := msync.NewTrigger()
+		checkNotActive(t, tr)
+
+		const numTasks = 5
+
+		ok := make([]bool, numTasks)
+		var start, stop sync.WaitGroup
+
+		for i := range numTasks {
+			start.Add(1)
+			stop.Add(1)
+			go func() {
+				ch := tr.Ready()
+				start.Done()
+				<-ch
+				ok[i] = true
+				stop.Done()
+			}()
+		}
+
+		// Wait until all the tasks have their channel.
+		start.Wait()
+
+		// Signal the trigger, and confirm that it is no longer active.
+		checkNotActive(t, tr)
+		tr.Signal()
+		checkNotActive(t, tr)
+
+		// Wait until all the tasks have completed.
+		stop.Wait()
+
+		for i, b := range ok {
+			if !b {
+				t.Errorf("Task %d did not report success", i+1)
+			}
+		}
+	})
+
+	t.Run("Set", func(t *testing.T) {
+		tr := msync.NewTrigger()
+		checkNotActive(t, tr)
+
+		// Verify that a goroutine that observes the trigger before the first set
+		// properly observes the activation later.
+		start := make(chan struct{})
+		done := make(chan struct{})
+		go func() {
+			ch := tr.Ready()
+			close(start)
+			<-ch
+			close(done)
+		}()
+		<-start
+
+		tr.Set()
+		tr.Set() // safe to do it multiple times
+		for i := range 3 {
+			select {
+			case <-tr.Ready():
+				t.Logf("OK, set trigger is active (check %d)", i+1)
+			case <-time.After(time.Second):
+				t.Error("Trigger is not ready when it should be")
+			}
+		}
+		select {
+		case <-done:
+			t.Log("OK, early observer saw the trigger fire")
+		case <-time.After(time.Second):
+			t.Error("Early observer did not see the activation")
+		}
+
+		tr.Reset()
+		checkNotActive(t, tr)
+		tr.Reset() // safe to do it multiple times
+		checkNotActive(t, tr)
+	})
 }
 
 func TestFlag(t *testing.T) {
--- trigger.go
+++ trigger.go
@@ -2,19 +2,23 @@ package msync
 
 import "sync"
 
-// Trigger is an edge-triggered condition shared by multiple goroutines.  It is
-// analogous in effect to the standard condition variable (sync.Cond) but uses
-// a channel for signaling.
+// Trigger is an edge-triggered condition shared by multiple goroutines.
+// The Ready method returns a channel that is closed when the trigger is
+// activated.
 //
-// To wait on the trigger, call Ready to obtain a channel. This channel will be
-// closed the next time the Signal method is executed. A fresh channel is
-// created after each call to Signal, and is shared among all calls to Ready
-// that occur in the window between signals.
+// When a trigger is first created it is inactive. It remains inactive until
+// the Set or Signal method is called, which causes the current ready channel
+// to be closed. Once a trigger has been activated, it remains active until it
+// is reset. Use the Reset method to reset the trigger to inactive.
+//
+// The Signal method immediately resets the trigger, acting as Set and Reset
+// done in a single step.
 //
 // A zero Trigger is ready for use but must not be copied after first use.
 type Trigger struct {
-	μ  sync.Mutex
-	ch chan struct{}
+	μ      sync.Mutex
+	ch     chan struct{}
+	closed bool
 
 	// The signal channel is lazily initialized by the first waiter.
 }
@@ -22,16 +26,40 @@ type Trigger struct {
 // NewTrigger constructs a new unready Trigger.
 func NewTrigger() *Trigger { return new(Trigger) }
 
-// Signal wakes all pending waiters and resets the trigger.
+// Signal activates and immediately resets the trigger.
 func (t *Trigger) Signal() {
 	t.μ.Lock()
 	defer t.μ.Unlock()
 
-	// If the channel is nil, there are no goroutines waiting.  The next waiter
-	// will update the channel (see Ready).
-	if t.ch != nil {
+	if t.ch != nil && !t.closed {
+		close(t.ch) // wake any pending waiters
+	}
+	t.ch = make(chan struct{})
+	t.closed = false
+}
+
+// Set activates the trigger, if it is not already active.
+func (t *Trigger) Set() {
+	t.μ.Lock()
+	defer t.μ.Unlock()
+
+	if t.ch == nil {
+		t.ch = make(chan struct{})
 		close(t.ch)
+	} else if !t.closed {
+		close(t.ch)
+	}
+	t.closed = true
+}
+
+// Reset resets the trigger.
+func (t *Trigger) Reset() {
+	t.μ.Lock()
+	defer t.μ.Unlock()
+
+	if t.closed {
 		t.ch = nil
+		t.closed = false
 	}
 }
 
@@ -40,10 +68,9 @@ func (t *Trigger) Ready() <-chan struct{} {
 	t.μ.Lock()
 	defer t.μ.Unlock()
 
-	// The first waiter after construction or a signal lazily populates the
-	// channel for the next period.
 	if t.ch == nil {
 		t.ch = make(chan struct{})
+		t.closed = false
 	}
 	return t.ch
 }
